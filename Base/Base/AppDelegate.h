0d76cd6592a7859ddc5d1394f5bd86b8bd3ea105
AdilVirani <adisv9997@gmail.com>
2015-01-25 17:43:18 -0600

    Added Base--Time to add pods

---
+++ Base/Base/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Base
+//
+//  Created by Adil  Virani on 1/25/15.
+//  Copyright (c) 2015 Adil  Virani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Base/Base/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Base
+//
+//  Created by Adil  Virani on 1/25/15.
+//  Copyright (c) 2015 Adil  Virani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
